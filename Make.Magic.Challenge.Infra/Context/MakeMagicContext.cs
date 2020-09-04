ad2411031f90dbc265a49e8fabb884a232c9ae86
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2020-09-04 06:32:51 -0300

    #4 - creating context

---
+++ Make.Magic.Challenge.Infra/Context/MakeMagicContext.cs
@@ -0,0 +1,63 @@
+﻿using BaseEntity.Domain.Entities;
+using Make.Magic.Challenge.Infra.Context.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Make.Magic.Challenge.Infra.Context
+{
+    public class MakeMagicContext : DbContext
+    {
+        public MakeMagicContext(DbContextOptions<MakeMagicContext> dbContextOptions)
+            : base(dbContextOptions) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            foreach (var type in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(Entity).IsAssignableFrom(type.ClrType) && (type.BaseType == null || !typeof(Entity).IsAssignableFrom(type.BaseType.ClrType)))
+                    modelBuilder.SetSoftDeleteFilter(type.ClrType);
+            }
+        }
+
+        public override int SaveChanges()
+        {
+            UpdateSoftDeleteStatuses();
+
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            UpdateSoftDeleteStatuses();
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        void UpdateSoftDeleteStatuses()
+        {
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (!(entry.Entity is Entity))
+                    continue;
+
+                switch (entry.State)
+                {
+                    case EntityState.Deleted:
+                        (entry.Entity as Entity).Delete();
+                        entry.State = EntityState.Modified;
+                        break;
+                }
+            }
+        }
+    }
+}
