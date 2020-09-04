9249a27232120d806b534a29b688475732702875
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2020-09-04 07:34:56 -0300

    #4 - first migration

---
+++ Make.Magic.Challenge.CrossCutting/DependencyInjector.cs
@@ -0,0 +1,18 @@
+﻿using Make.Magic.Challenge.Domain.Character.Repositories.Contracts;
+using Make.Magic.Challenge.Infra.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Make.Magic.Challenge.CrossCutting
+{
+    public class DependencyInjector
+    {
+        public static void Configure(IServiceCollection services)
+        {
+            #region ' Character '
+
+            services.AddTransient<ICharacterRepository, CharacterRepository>();
+
+            #endregion
+        }
+    }
+}
---
+++ Make.Magic.Challenge.Infra/Migrations/20200904103241_Migration_01.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Make.Magic.Challenge.Infra.Migrations
+{
+    public partial class Migration_01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Character",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Code = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    LastUpdate = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Role = table.Column<string>(nullable: true),
+                    School = table.Column<string>(nullable: true),
+                    House = table.Column<string>(nullable: true),
+                    Patronus = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Character", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Character");
+        }
+    }
+}
